5a78821fc75247c854ca158fe77c817bdc5187ec
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-08-06 20:55:23 +0200

    ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,29 +1,10 @@
-#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
 int main(){
 
-    std::cout << "===Constuctor===" << std::endl;
-    ClapTrap    a;
-    ClapTrap    b("La tueur");
-
-    std::cout << "===Action Normal===" << std::endl;
-    a.attack("Default");
-    b.takeDamage(3);
-    b.beRepaired(2);
-
-    std::cout << "===Plus energie===" << std::endl;
-    for (int i = 0; i < 10; i++){
-        b.attack("Alfred");
-    }
-
-    b.attack("Bradley");
-    b.beRepaired(5);
-
-    std::cout << "===Plus de HP===" << std::endl;
-    a.takeDamage(20);
-
-    a.attack("Divina");
-    a.beRepaired(5);
+    ScavTrap objet("Bob");
+    objet.attack("cible");
+    objet.guardGate();
 
     return (0);
 }
